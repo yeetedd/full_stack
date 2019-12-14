bb27916698b8438ab116f8b66d04a24f0ca414e7
Yeet <pirminkki2gmail.com>
2019-12-14 17:07:14 +0200

    jej

--- public/js/elokuva.js
+++ public/js/elokuva.js
@@ -1,11 +1,32 @@
-function haeElokuva(){
+haeArvosteludata();
 
-   haeData();
-
+async function haeArvosteludata(){
+  const response = await fetch('/api/arvostelut')
+  const data = await response.json();
+  console.log(data);
+  console.log(data[0]);
+  taytaArvostelutaulukko(data);
 }
 
+function taytaArvostelutaulukko(data){
+  var table = document.getElementById("arvostelutaulukko");
+  for (var i = 0; i < data.length; i++) {
+    var row = table.insertRow(i + 1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = data[i].leffa;
+    cell2.innerHTML = data[i].arvostelu;
+    cell3.innerHTML = data[i].arvostelija;
+  }
+}
 
-async function haeData(){
+function haeElokuva(){
+
+  haeElokuvaData();
+}
+
+async function haeElokuvaData(){
 
   var leffan_nimi = document.getElementById("elokuva").value;
   var leffan_vuosi = document.getElementById("vuosi").value;
@@ -22,3 +43,30 @@ async function haeData(){
   poster.src = data.Poster;
 
 }
+
+
+function arvosteleElokuva(){
+  var leffa = document.getElementById("elokuva").value;
+  var arvostelut = document.getElementById("arvostelu").value;
+  var arvostelija = document.getElementById("arvostelija").value;
+
+  const data = {leffa, arvostelu, arvostelija};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch('/api/arvostelut', options).then(function(response){
+    console.log(response)
+    if(response.status == 201){
+      console.log("PUSH OK!")
+    }
+  }, function(error){
+    console.log(error.message);
+  });
+}
+
+//haeArvosteludata();
--- server.js
+++ server.js
@@ -1,6 +1,47 @@
 const express = require('express')
 const app = express();
+const db = require('./queries')
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("kuuntelen" + port));
+app.listen(port, () => console.log("kuuntelen porttia" + " " + port));
+
 app.use(express.static("public"));
+
+//app.use(express.json({limit: '1mb'}));
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+const arvostelut = [
+  {
+    "leffa":"Titanik",
+    "arvostelu":"Klassikkodraamaa",
+    "arvostelija":"mika",
+  },
+  {
+    "leffa":"starwars",
+    "arvostelu":"silmäkarkkia",
+    "arvostelija":"suvi"
+  }
+]
+
+app.get('/api/arvostelut/vanha', function (recuest, response){
+  response.send(arvostelut);
+})
+
+app.post('/api/arvostelu/vanha/vanha', function (recuest, response){
+  console.log("leffan arvostelu");
+  console.log(recuest.body);
+  arvostelut.push(recuest.body);
+  console.log(arvostelut);
+  response.send(200);
+})
+
+app.get('/api/arvostelut', db.haeArvostelut)
+app.post('/api/arvostelu', db.luoArvostelu)
